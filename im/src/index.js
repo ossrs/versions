db7b1fadc47cb1a548216bb30b7de24d71dc34dc
winlin <winlin@vip.126.com>
2021-07-05 07:35:06 +0800

    Update

--- im/src/index.js
+++ im/src/index.js
@@ -2,17 +2,21 @@
 
 const im = require('./im.server')
 const TLSSigAPIv2 = require('tls-sig-api-v2')
-const sdk = im.create(process.env.IM_SDKAPPID, process.env.IM_SECRETKEY, process.env.IM_ADMINISTRATOR)
+const SDKAppID = parseInt(process.env.IM_SDKAPPID)
+const sdk = im.create(SDKAppID, process.env.IM_SECRETKEY, process.env.IM_ADMINISTRATOR)
 
 exports.main_handler = async (event, context) => {
     const q = event.queryString
     console.log("Hello World", event, q)
 
     if (event.path === '/im-service/v1/login') {
+        // Register as IM user, nickName and faceUrl is optional.
+        await sdk.account_import(q.user, q.nickName, q.faceUrl)
+
         return {
-            user_id: q.user,
-            user_sig: new TLSSigAPIv2.Api(parseInt(process.env.IM_SDKAPPID), process.env.IM_SECRETKEY).genSig(q.user, 1 * 24 * 3600),
-            res: await sdk.account_import(q.user, q.nickName, q.faceUrl),
+            SDKAppID: SDKAppID,
+            userID: q.user,
+            userSig: new TLSSigAPIv2.Api(SDKAppID, process.env.IM_SECRETKEY).genSig(q.user, 1 * 24 * 3600),
         }
     }
 
