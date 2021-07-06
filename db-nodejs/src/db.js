2dca18db45be71b4812621480eb294620eaeb12f
winlin <winlin@vip.126.com>
2021-07-06 13:03:42 +0800

    Update

--- db-nodejs/src/db.js
+++ db-nodejs/src/db.js
@@ -53,9 +53,20 @@ const initialize = async () => {
       ) ENGINE=InnoDB DEFAULT CHARSET=utf8
     `)
 
+    // Create and update user.
     await conn.query(
-      'INSERT IGNORE INTO admins(userName, password) VALUES(?, ?)', 
-      [String(process.env.SRS_ADMIN),String(process.env.SRS_PASSWORD)]
+      'INSERT IGNORE INTO admins(userName, password) VALUES(?, ?),(?, ?)', [
+        String(process.env.SRS_ADMIN), String(process.env.SRS_PASSWORD),
+        String(process.env.SRS_ADMIN1), String(process.env.SRS_PASSWORD1),
+      ]
+    )
+    await conn.query(
+      'UPDATE admins SET password=? WHERE userName=?',
+      [String(process.env.SRS_PASSWORD), String(process.env.SRS_ADMIN)]
+    )
+    await conn.query(
+      'UPDATE admins SET password=? WHERE userName=?',
+      [String(process.env.SRS_PASSWORD1), String(process.env.SRS_ADMIN1)]
     )
   } finally {
     conn.end()
